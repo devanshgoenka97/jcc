0a47e5d805f881b13269b6bf78f767b7223c59ea
Devansh Goenka <devanshgoenka97@gmail.com>
2017-10-30 00:52:22 +0530

    Added phone number and team name check:

--- myapp/views.py
+++ myapp/views.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.core.context_processors import csrf
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
+import re
 import hashlib
 import urllib2, urllib
 
@@ -22,6 +23,10 @@ def encode_base(num, array=ARR):
     retarr.reverse()
     return ''.join(retarr)[:6]
 
+def validate_number(num):
+    pattern = re.compile("^\d{10}$",re.IGNORECASE)
+    return pattern.match(num) is not None
+
 def index(request):
     c = {}
     c.update(csrf(request))
@@ -32,15 +37,24 @@ def shorten_url(request):
      p1 = request.POST.get("c1", '')
      p2 = request.POST.get("c2", '')
      cont = request.POST.get("phone", '')
+     error_data={}
+     error_data['err'] = "Blank field(s)"
      if team=='' :
-        return HttpResponse(json.dumps({"error": "error occurs"}), content_type="application/json")
+        return HttpResponse(json.dumps(error_data), content_type="application/json")
      if p1=='' :
-        return HttpResponse(json.dumps({"error": "error occurs"}), content_type="application/json")
+        return HttpResponse(json.dumps(error_data), content_type="application/json")
      if p2=='' :
-        return HttpResponse(json.dumps({"error": "error occurs"}), content_type="application/json")
+        return HttpResponse(json.dumps(error_data), content_type="application/json")
      if cont=='' :
-        return HttpResponse(json.dumps({"error": "error occurs"}), content_type="application/json")
-     alphanum=team +cont
+        return HttpResponse(json.dumps(error_data), content_type="application/json")
+     alphanum=team + cont
+     team_name = urls.objects.filter(teamname=team)
+     if team_name.count()>=1:
+        error_data['err'] = "Team name already exists!"
+        return HttpResponse(json.dumps(error_data), content_type="application/json")
+     if not validate_number(cont):
+        error_data['err'] = "Please enter a valid number!"
+        return HttpResponse(json.dumps(error_data), content_type="application/json")
      short = (hashlib.md5(alphanum.encode())).hexdigest()
      short = int(short,16)
      short = encode_base(short)
